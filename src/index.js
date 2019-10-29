bab5b0249cea30074a64f37d4c4e8e4e2f45b91d
adryanyudhatrisna <ayudhatrisna1989@gmail.com>
2019-10-28 21:42:38 -0400

    Added some styles to our chat application

--- src/index.js
+++ src/index.js
@@ -2,32 +2,53 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
+const {
+  generateMessage,
+  generateLocationMessage
+} = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 const port = process.env.PORT || 3000;
-const publicDirectory = path.join(__dirname, '../public');
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.use(express.static(publicDirectory));
-
-let count = 0;
+app.use(express.static(publicDirectoryPath));
 
 io.on('connection', socket => {
-  console.log('New socket connection');
+  console.log('New WebSocket connection');
 
-  socket.emit('message', 'Welcome is my chat app');
-  socket.broadcast.emit('message', 'A new user has joined');
+  socket.emit('message', generateMessage('Welcome!'));
+  socket.broadcast.emit('message', generateMessage('A new user has joined!'));
 
-  socket.on('sendMessage', message => {
-    io.emit('message', message);
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed!');
+    }
+
+    io.emit('message', generateMessage(message));
+    callback();
   });
+
+  socket.on('sendLocation', (coords, callback) => {
+    io.emit(
+      'locationMessage',
+      generateLocationMessage(
+        `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+      )
+    );
+    callback();
+  });
+
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left');
+    io.emit('message', generateMessage('A user has left!'));
   });
 });
 
 server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is up on port ${port}!`);
 });
