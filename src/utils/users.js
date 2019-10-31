5547bac7b1e1ba1f3dd0be5c811676c871569636
adryanyudhatrisna <ayudhatrisna1989@gmail.com>
2019-10-30 21:19:26 -0400

    Finished the application

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,54 +1,55 @@
-const users = [];
-
-// addUser, removeUser, getUser, getUsersInRoom
+const users = []
 
 const addUser = ({ id, username, room }) => {
-  // Clean the data
-  username = username.trim().toLowerCase();
-  room = room.trim().toLowerCase();
+    // Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
 
-  // Validate the data
-  if (!username || !room) {
-    return {
-      error: 'Username and room are required!'
-    };
-  }
+    // Validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
 
-  // Check for existing user
-  const existingUser = users.find(user => {
-    return user.room === room && user.username === username;
-  });
+    // Check for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
 
-  // Validate username
-  if (existingUser) {
-    return {
-      error: 'Username is in use!'
-    };
-  }
+    // Validate username
+    if (existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
 
-  // Store user
-  const user = { id, username, room };
-  users.push(user);
-  return { user };
-};
+    // Store user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
 
-const removeUser = id => {
-  const index = users.findIndex(user => user.id === id);
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
 
-  if (index !== -1) {
-    return users.splice(index, 1)[0];
-  }
-};
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
 
-addUser({
-  id: 22,
-  username: 'Adryan  ',
-  room: '  Columbus'
-});
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
 
-console.log(users);
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
+}
 
-const removedUser = removeUser(22);
-
-console.log(removedUser);
-console.log(users);
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
